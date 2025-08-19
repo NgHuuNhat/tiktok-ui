6a628abd55c1f6a4f505fbd0266110c1d76ff311
Ng Huu Nhat <nhat200901@gmail.com>
2025-08-19 23:15:53 +0700

    update

--- my-app/src/components/Layout/HeaderOnly/index.tsx
+++ my-app/src/components/Layout/HeaderOnly/index.tsx
@@ -3,7 +3,7 @@ import Header from '../components/Header'
 
 export default function HeaderOnly({ children }: any) {
     return (
-        <div>
+        <div className='header-only'>
             <Header />
             <div className='container'>
                 <div className='content'>
